4564f22a34afd513534996cd1570d7503c30bc4e
David Moreau <dmoreau@lamobilery.fr>
2020-09-18 22:37:44 +0200

    Added add workouts form validation

---
+++ front/src/interfaces/WorkoutFields.ts
@@ -0,0 +1,7 @@
+export interface WorkoutFields {
+  spm: string
+  time: string
+  distance: string
+  kcal: string
+  date: string
+}
--- front/src/pages/WorkoutFormPage.tsx
+++ front/src/pages/WorkoutFormPage.tsx
@@ -1,7 +1,9 @@
 import { Box, Button, makeStyles, Paper, TextField } from '@material-ui/core'
 import { lightFormat } from 'date-fns'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import DashboardLayout from '../components/DashboardLayout'
+import { WorkoutFields } from '../interfaces/WorkoutFields'
 import { getDefaultDateFormat } from '../utils/date-format'
 
 const useStyles = makeStyles({
@@ -23,51 +25,63 @@ const useStyles = makeStyles({
 
 const WorkoutFormPage: React.FC = () => {
   const styles = useStyles()
+  const onSubmit = (data: any) => console.log(data)
+  const { handleSubmit, register } = useForm<WorkoutFields>()
 
   return (
     <DashboardLayout>
       <Paper className={styles.paper}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <Box className={styles.fieldBox}>
             <TextField
-              id="spm"
-              fullWidth
+              name="spm"
               type="number"
               label="SPM moyen"
-              value="0"
+              fullWidth
+              required={true}
+              inputRef={register}
             />
           </Box>
           <Box className={styles.fieldBox}>
             <TextField
-              id="time"
-              fullWidth
+              name="time"
               type="number"
               label="Temps de la séance (en min)"
+              fullWidth
+              required={true}
+              inputRef={register}
             />
           </Box>
           <Box className={styles.fieldBox}>
             <TextField
-              id="distance"
+              name="distance"
               fullWidth
               type="number"
               label="Distance (en KM)"
+              inputProps={{ step: 0.1 }}
+              required={true}
+              inputRef={register}
             />
           </Box>
           <Box className={styles.fieldBox}>
             <TextField
-              id="kcal"
+              name="kcal"
               fullWidth
               type="number"
               label="Calories (en kcal)"
+              required={true}
+              inputRef={register}
             />
           </Box>
           <Box className={styles.fieldBox}>
             <TextField
-              id="date"
+              name="date"
               fullWidth
               type="date"
               label="Date"
+              required={true}
               defaultValue={lightFormat(new Date(), getDefaultDateFormat())}
+              inputRef={register}
             />
           </Box>
           <Box className={styles.submitButton}>
